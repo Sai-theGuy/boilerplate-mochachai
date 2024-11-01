d8eba7e41b37fb8d428740a1b2d0f02b38a349f5
Glitch (ambitious-lilac-decision) <none>
2024-11-01 15:52:10 +0000

    Finished Headless HTTP w zombie

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -84,20 +84,30 @@ suite("Functional Tests with Zombie.js", function () {
       assert.isNotNull(browser.site);
     });
   });
-
   suite('"Famous Italian Explorers" form', function () {
     // #5
     test('Submit the surname "Colombo" in the HTML form', function (done) {
-      assert.fail();
-
+      browser.fill("#i1", "Colombo").then(() => {
+        browser.pressButton("success", () => {
+          browser.assert.success();
+          browser.assert.text("span#name", "Cristoforo");
+          browser.assert.text("span#surname", "Colombo");
+          browser.assert.elements("span#dates", 1);
+        });
+      });
+      done();
+    });
+    // #6
+    test('Submit the surname "Vespucci" in the HTML form', function (done) {
+      browser.fill("#i1", "Vespucci").then(() => {
+        browser.pressButton("success", () => {
+          browser.assert.success();
+          browser.assert.text("span#name", "Amerigo");
+          browser.assert.text("span#surname", "Vespucci");
+          browser.assert.elements("span#dates", 1);
+        });
+      });
       done();
     });
   });
-  //     // #6
-  //     test('Submit the surname "Vespucci" in the HTML form', function (done) {
-  //       assert.fail();
-
-  //       done();
-  //     });
-  //   });
 });
